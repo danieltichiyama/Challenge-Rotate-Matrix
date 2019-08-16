63ef3467badd2fd17b140594663692c4e3ea3687
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-08-15 18:52:38 -1000

    finished basic, ready for review

--- MatrixRotator.js
+++ MatrixRotator.js
@@ -11,16 +11,36 @@ const Direction = require("./Direction").Direction;
  *        @returns the rotated matrix
  */
 module.exports = class MatrixRotator {
-  constructor( matrix ) {
+  constructor(matrix) {
     this.matrix = matrix;
   }
 
   //      |-- Must be Direction.CW
   //      v        or Direction.CCW
-  rotate (direction) {
+  rotate(direction) {
     // do work here
+    let newMatrix = [];
+
+    if (direction === Direction.CW) {
+      for (let i = 0; i < this.matrix.length; i++) {
+        let newRow = [];
+        for (let j = this.matrix[i].length - 1; j >= 0; j--) {
+          newRow.push(this.matrix[j][i]);
+        }
+        newMatrix.push(newRow);
+      }
+    } else if (direction === Direction.CCW) {
+      for (let i = this.matrix.length - 1; i >= 0; i--) {
+        let newRow = [];
+        for (let j = 0; j < this.matrix[i].length; j++) {
+          newRow.push(this.matrix[j][i]);
+        }
+        newMatrix.push(newRow);
+      }
+    }
+
+    this.matrix = newMatrix;
 
     // must be a valid Direction, see Direction.js
-
   }
 };
